d5128b8ad1948e8b2a3ccdb1efb213f8b856ef70
ratuajengg <160453692+ratuajengg@users.noreply.github.com>
2024-02-20 14:35:05 +0700

    Add files via upload

--- sketch2.js
+++ sketch2.js
@@ -3,7 +3,7 @@ let z;
 
 function setup(){
     createCanvas(600,400);
-    background(3,172,240);
+    
     x = 0;
     x1= 100;
     j = 100;
@@ -13,6 +13,14 @@ function setup(){
 }   
 
 function draw(){
+   
+   
+    var x = 450 + 40 * Math.sin(PI/100*j)
+    j+=1
+    background(3,172,240);
+    triangle(450,130,x,180,450,190);
+    fill(210,105,30);
+    
     line(300,600,300,0); 
     line(150,130,150,220);
     line(450,130,450,220);
@@ -21,12 +29,7 @@ function draw(){
     line(450,130,400,220);
     line(450,130,500,220);
     triangle(150,130,190,180,150,190);
-   
-    var x = 450 + 50 * Math.sin(PI/50*j)
-    j+=1
-    fill(210,105,30);
-    triangle(450,130,x,180,450,190);
-   
+    
     beginShape();
     vertex(350, 220);
     vertex(400, 260);
